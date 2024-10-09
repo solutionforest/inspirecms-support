103d48baaf02139a922e5dce92ee015a622f2323
carly <carly.cheung@solutionforest.net>
2024-10-09 15:12:08 +0800

    fix layout

--- resources/views/components/tree-node/index.blade.php
+++ resources/views/components/tree-node/index.blade.php
@@ -3,10 +3,23 @@
     {{ $attributes->merge([
         'class' => 'tree-node-layout',
     ]) }}
+    x-data="{ isExpandedSidebar: false }"
 >
-
-    <div class="tree-node-sidebar">
-        {{ $slot }}
+    <div class="tree-node-sidebar-container">
+        <x-filament::icon-button
+            color="gray"
+            icon="heroicon-o-bars-3"
+            icon-size="md"
+            x-cloak
+            @click="isExpandedSidebar = !isExpandedSidebar"
+            class="p-2"
+        />
+        <div class="tree-node-sidebar"
+            x-show="isExpandedSidebar"
+            x-cloak
+        >
+            {{ $slot }}
+        </div>
     </div>
     
     <div class="tree-node-main">
