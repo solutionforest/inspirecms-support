7ef58a58c4eee2e81b5c65c6ebd563bbcd5ebd9b
cklei-carly <cklei-carly@users.noreply.github.com>
2024-10-17 10:42:06 +0000

    Fix styling

--- src/UrlGenerators/ContentUrlGenerator.php
+++ src/UrlGenerators/ContentUrlGenerator.php
@@ -4,6 +4,4 @@ namespace SolutionForest\InspireCms\Support\UrlGenerators;
 
 use SolutionForest\InspireCms\Support\Base\UrlGenerators\BaseContentUrlGenerator;
 
-class ContentUrlGenerator extends BaseContentUrlGenerator implements ContentUrlGeneratorInterface
-{
-}
+class ContentUrlGenerator extends BaseContentUrlGenerator implements ContentUrlGeneratorInterface {}
