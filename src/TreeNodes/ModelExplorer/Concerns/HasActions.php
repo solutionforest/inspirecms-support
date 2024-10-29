9d4380576e3c619b3ca81391f3e01128ac05d7ea
carly <carly.cheung@solutionforest.net>
2024-10-29 13:17:06 +0800

    wip: config actions

--- src/TreeNodes/ModelExplorer/Concerns/HasActions.php
+++ src/TreeNodes/ModelExplorer/Concerns/HasActions.php
@@ -36,14 +36,13 @@ trait HasActions
     public function pushActions(array $actions): static
     {
         foreach (Arr::wrap($actions) as $action) {
-
-            if ($action instanceof Action) {
+        if ($action instanceof Action) {
                 $action->defaultSize(ActionSize::Small);
                 $action->defaultView($action::LINK_VIEW);
 
                 $this->cacheAction($action);
             } else {
-                throw new \InvalidArgumentException('Table actions must be an instance of ' . Action::class . ' or ' . ActionGroup::class . '.');
+                throw new \InvalidArgumentException('The actions must be an instance of ' . Action::class . '.');
             }
 
             $this->actions[] = $action;
@@ -53,42 +52,11 @@ trait HasActions
     }
 
     /**
-     * @return array<Action | ActionGroup>
+     * @return array<Action>
      */
     public function getActions(): array
     {
         return $this->actions;
-    }
-
-    /**
-     * @param  string | array<string>  $name
-     */
-    public function getAction(string | array $name): ?Action
-    {
-        if (is_string($name) && str($name)->contains('.')) {
-            $name = explode('.', $name);
-        }
-
-        if (is_array($name)) {
-            $firstName = array_shift($name);
-            $modalActionNames = $name;
-
-            $name = $firstName;
-        }
-
-        $mountedRecord = $this->getLivewire()->getMountedTableActionRecord();
-
-        $action = $this->getFlatActions()[$name] ?? null;
-
-        if (! $action) {
-            return null;
-        }
-
-        return $this->getMountableModalActionFromAction(
-            $action->record($mountedRecord),
-            modalActionNames: $modalActionNames ?? [],
-            mountedRecord: $mountedRecord,
-        );
     }
 
     /**
@@ -111,63 +79,5 @@ trait HasActions
         } else {
             $this->flatActions[$action->getName()] ??= $action;
         }
-    }
-
-    /**
-     * @param  array<string, Action>  $actions
-     */
-    protected function mergeCachedFlatActions(array $actions, bool $shouldOverwriteExistingActions = true): void
-    {
-        if ($shouldOverwriteExistingActions) {
-            $this->flatActions = [
-                ...$this->flatActions,
-                ...$actions,
-            ];
-        } else {
-            $this->flatActions = [
-                ...$actions,
-                ...$this->flatActions,
-            ];
-        }
-    }
-
-    /**
-     * @param  array<string>  $modalActionNames
-     */
-    protected function getMountableModalActionFromAction(Action $action, array $modalActionNames, ?Model $mountedRecord = null): ?Action
-    {
-        $arguments = $this->getLivewire()->mountedTableActionsArguments ?? [];
-
-        if (
-            (($actionArguments = array_shift($arguments)) !== null) &&
-            (! $action->hasArguments())
-        ) {
-            $action->arguments($actionArguments);
-        }
-
-        foreach ($modalActionNames as $modalActionName) {
-            $action = $action->getMountableModalAction($modalActionName);
-
-            if (! $action) {
-                return null;
-            }
-
-            if ($action instanceof Action) {
-                $action->record($mountedRecord);
-            }
-
-            if (
-                (($actionArguments = array_shift($arguments)) !== null) &&
-                (! $action->hasArguments())
-            ) {
-                $action->arguments($actionArguments);
-            }
-        }
-
-        if (! $action instanceof Action) {
-            return null;
-        }
-
-        return $action;
     }
 }
