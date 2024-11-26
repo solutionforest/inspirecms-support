0588f17793fa1ac65084a3efb64b844f39e47c29
cklei-carly <cklei-carly@users.noreply.github.com>
2024-11-26 03:37:01 +0000

    Fix styling

--- src/Base/Manifests/ModelRegistryInterface.php
+++ src/Base/Manifests/ModelRegistryInterface.php
@@ -4,11 +4,10 @@ namespace SolutionForest\InspireCms\Support\Base\Manifests;
 
 interface ModelRegistryInterface
 {
-
     /**
      * Retrieve an instance of the specified interface class.
      *
-     * @param string $interfaceClass The fully qualified name of the interface class to retrieve.
+     * @param  string  $interfaceClass  The fully qualified name of the interface class to retrieve.
      * @return ?string The instance of the specified interface class.
      */
     public function get(string $intefaceClass);
@@ -16,8 +15,8 @@ interface ModelRegistryInterface
     /**
      * Replaces the existing model class associated with the given interface class.
      *
-     * @param string $interfaceClass The fully qualified name of the interface class.
-     * @param string $modelClass The fully qualified name of the new model class to associate with the interface.
+     * @param  string  $interfaceClass  The fully qualified name of the interface class.
+     * @param  string  $modelClass  The fully qualified name of the new model class to associate with the interface.
      * @return void
      */
     public function replace(string $intefaceClass, string $modelClass);
