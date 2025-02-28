6cf8375b1f28857be3131d09fa0c634e0554e603
carly <carly.cheung@solutionforest.net>
2025-02-28 19:04:51 +0800

    fix: console.log for media picker

--- resources/views/forms/components/media-picker/modal.blade.php
+++ resources/views/forms/components/media-picker/modal.blade.php
@@ -12,52 +12,36 @@
     footer-actions-alignment="end"
     width="screen"
     class="media-library-browser-modal"
-    x-data="{ 
-        selected: [], 
-        formStatePath: '', 
-        modalInitialized: false,
-
-        closeMediaPickerModal: function(save = true) {
-            if (save) {
-                this.$dispatch('close-modal', { id: '{{ $modalId }}', save: true, statePath: this.formStatePath, data: { selected: this.selected } });
-            } else {
-                this.$dispatch('close-modal', { id: '{{ $modalId }}', save: false, statePath: this.formStatePath });
-            }
-        },
-        onMediaPickerModalSetup: function($event) {
-            this.selected = $event.detail.selected ?? [];
-            this.formStatePath = $event.detail.statePath ?? '';
-            this.modalInitialized = false;
-
-            this.$dispatch('media-picker-modal:init', { config: $event.detail?.config ?? [] });
-        },
-
-        onMediaPickerModalSetupComplete: function($event) {
-            this.modalInitialized = true;
-        },
-        
+    x-init="() => {
+        this.selected = [];
+        this.formStatePath = false;
+        this.modalInitialized = false;
     }"
-    x-on:media-picker-setup.window="onMediaPickerModalSetup($event)"
-    x-on:media-picker-modal-setup-complete="onMediaPickerModalSetupComplete($event)"
+    x-on:media-picker-setup.window="(event) => {
+
+        this.selected = event.detail.selected ?? [];
+        this.formStatePath = event.detail.statePath ?? '';
+        this.modalInitialized = false;
+
+        $dispatch('media-picker-modal:init', { config: event.detail?.config ?? [] });
+    }"
+    x-on:media-picker-modal-setup-complete="() => {
+        this.modalInitialized = true;
+    }"
 >
     <x-slot name="heading">
         {{ __('inspirecms-support::actions.select.modal.heading') }}
     </x-slot>
 
-    <div x-show="!modalInitialized">
-        Loading...
-    </div>
-    <div x-show="modalInitialized">
-        <livewire:inspirecms-support::media-library
-            is-modal-picker="true"
-        />
-    </div>
+    <livewire:inspirecms-support::media-library
+        is-modal-picker="true"
+    />
 
     <x-slot name="footerActions">
-        <x-filament::button x-on:click="closeMediaPickerModal(true)">
+        <x-filament::button x-on:click="$dispatch('close-modal', { id: '{{ $modalId }}', save: true, statePath: this.formStatePath, data: { selected: this.selected } })">
             {{ __('inspirecms-support::actions.select.label') }}
         </x-filament::button>
-        <x-filament::button color="gray" x-on:click="closeMediaPickerModal(false)">
+        <x-filament::button color="gray" x-on:click="$dispatch('close-modal', { id: '{{ $modalId }}', save: false, statePath: this.formStatePath })">
             {{ __('inspirecms-support::actions.cancel.label') }}
         </x-filament::button>
     </x-slot>
