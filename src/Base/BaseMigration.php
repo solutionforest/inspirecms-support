78cbbc77e3b9099d4ef1d14e8a7f7ca9ca9fd3d8
carly <carly.cheung@solutionforest.net>
2025-08-20 16:12:34 +0800

    hotfix: migration table names

--- src/Base/BaseMigration.php
+++ src/Base/BaseMigration.php
@@ -13,4 +13,32 @@ abstract class BaseMigration extends Migration
     {
         $this->prefix = ModelRegistry::getTablePrefix();
     }
+
+    protected function getCmsTableNames()
+    {
+        return [
+            'allowed_document_type' => $this->prefix . 'document_type_allowed_document_type',
+            'content_lock' => $this->prefix . 'content_locks',
+            'content_path' => $this->prefix . 'content_paths',
+            'content_publish_version' => $this->prefix . 'content_publish_version',
+            'content_route' => $this->prefix . 'content_routes',
+            'content_version' => $this->prefix . 'content_versions',
+            'content_web_setting' => $this->prefix . 'content_web_settings',
+            'content' => $this->prefix . 'content',
+            'document_type_inheritance' => $this->prefix . 'document_type_inheritance',
+            'document_type' => $this->prefix . 'document_types',
+            'export' => $this->prefix . 'exports',
+            'field_groupable' => $this->prefix . 'field_groupables',
+            'import' => $this->prefix . 'imports',
+            'key_value' => $this->prefix . 'key_values',
+            'language' => $this->prefix . 'languages',
+            'media_asset' => $this->prefix . 'media_assets',
+            'navigation' => $this->prefix . 'navigation',
+            'sitemap' => $this->prefix . 'sitemaps',
+            'templatable' => $this->prefix . 'templateable',
+            'template' => $this->prefix . 'templates',
+            'user_login_activity' => $this->prefix . 'user_login_activities',
+            'user' => $this->prefix . 'users',
+        ];
+    }
 }
