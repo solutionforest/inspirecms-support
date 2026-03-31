537c89d99a7bd64c8b857ea8b18faa1e1f311c48
cklei-carly <carly.cheung@solutionforest.net>
2026-03-31 15:24:37 +0800

    Update deps and refresh media library UI
    Bump multiple JS dependencies in package-lock.json (Tailwind packages, @parcel/watcher, @jridgewell/*, sourcemap tools, etc.), add license metadata and update optional/engine fields where applicable. Rebuilds/compiled CSS updated for the media library (resources/css/components/media-library.css and corresponding files in resources/dist) and update tree-node stylesheet. Small change to MediaPicker PHP component (src/MediaLibrary/Forms/Components/MediaPicker.php) to accompany the UI/style updates.

--- src/MediaLibrary/Forms/Components/MediaPicker.php
+++ src/MediaLibrary/Forms/Components/MediaPicker.php
@@ -151,7 +151,7 @@ class MediaPicker extends Field
                 'selection' => $this->getState() ?? [],
             ])
             ->extraModalWindowAttributes([
-                'class' => 'media-library-browser-modal-content',
+                'class' => 'media-library-browser-modal',
             ])
             ->modalHeading(__('inspirecms-support::media-library.buttons.select.heading'))
             ->modalSubmitActionLabel(__('inspirecms-support::media-library.buttons.select.label'))
